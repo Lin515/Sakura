b5411b5776e964bda1bac078eb527b2ca5f87151
LinGe <35224561+Lin515@users.noreply.github.com>
2022-06-01 11:26:12 +0800

    使用压缩后的 waifu-tips.min.js

--- cdn/live2d-widget/autoload.js
+++ cdn/live2d-widget/autoload.js
@@ -24,7 +24,7 @@ if (screen.width >= 768) {
 	Promise.all([
 		loadExternalResource(live2d_path + "waifu.css", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
-		loadExternalResource(live2d_path + "waifu-tips.js", "js")
+		loadExternalResource(live2d_path + "waifu-tips.min.js", "js")
 	]).then(() => {
 		/* 可直接修改部分参数 */
 		live2d_settings['showToolMenu'] = false; // 不显示工具栏
